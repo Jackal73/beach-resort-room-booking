4c01c2b5b56e1939c71b49495d4a8b17f2c7f102
Shawn Kebel <fxrender@outlook.com>
2021-04-08 18:55:10 -0500

    Add RoomContaine,r RoomFilter, and RoomList functional components

---
+++ src/components/RoomFilter.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function RoomFilter() {
+  return <div>hello from room filter</div>;
+}
---
+++ src/components/RoomList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Room from "./Room";
+
+export default function RoomList({ rooms }) {
+  if (rooms.length === 0) {
+    return (
+      <div className="empty-search">
+        Unfortunately, there were no rooms that matched your search criteria
+      </div>
+    );
+  }
+  return (
+    <section className="roomslist">
+      <div className="roomslist-center">
+        {rooms.map((item) => {
+          return <Room key={item.id} room={item} />;
+        })}
+      </div>
+    </section>
+  );
+}
